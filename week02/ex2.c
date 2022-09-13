c114cc41ec2daabf3717e92d9001895162b75ef6
RinRi <rin@rinri-d.xyz>
2022-09-13 21:59:22 +0300

    fix ex2

--- week02/ex2.c
+++ week02/ex2.c
@@ -12,7 +12,9 @@ int main() {
     s[i] = '\0';
     // i is already the size of s. I've used strlen to follow the 'hints'
     // section.
+    putc('\"', stdout);
     for (i = strlen(s) - 1; i >= 0; --i) {
         putc(s[i], stdout);
     }
+    putc('\"', stdout);
 }
